ee2b72ea4481844d878945e4bbe49aea212a59de
thomast1906 <thomast1906@users.noreply.github.com>
2024-02-26 21:29:22 +0000

    Terraform fmt

--- modules/virtual_network/subnets.tf
+++ modules/virtual_network/subnets.tf
@@ -1,10 +1,10 @@
 resource "azurerm_subnet" "additional_subnets" {
   for_each = { for subnet in var.subnets : subnet.name => subnet }
 
-  name                                           = each.value.name
-  address_prefixes                               = [each.value.address_prefix]
-  resource_group_name                            = var.resource_group_name
-  virtual_network_name                           = azurerm_virtual_network.virtual_network.name
+  name                                      = each.value.name
+  address_prefixes                          = [each.value.address_prefix]
+  resource_group_name                       = var.resource_group_name
+  virtual_network_name                      = azurerm_virtual_network.virtual_network.name
   private_endpoint_network_policies_enabled = each.value.enable_pe_policy
 
   service_endpoints = each.value.service_endpoints
