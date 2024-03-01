a4d19dded91ddc6c4eb09de1031b11f6a2fd5a6c
Thomas Thornton <>
2024-03-01 09:00:25 +0000

    fmt issues

--- modules/virtual_network/outputs.tf
+++ modules/virtual_network/outputs.tf
@@ -2,7 +2,7 @@ output "network_id" {
   value = azurerm_virtual_network.virtual_network.id
 }
 
-output "network_name" {
+output    "network_name" {
   value = azurerm_virtual_network.virtual_network.name
 }
 
